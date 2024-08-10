11ab49614cfab70189d249c6aa6754648d13b535
maruthivadde <vadde.maruthi123@gmail.com>
2024-08-10 11:18:31 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [message, setMessage] = useState('');
+  const [nextNumbers, setNextNumbers] = useState([]);
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+
+    if (isNaN(value)) {
+      setMessage('');
+      setNextNumbers([]);
+      return;
+    }
+
+    setInputValue(e.target.value);
+
+    if (value < 0) {
+      setMessage('Enter a positive value');
+      setNextNumbers([]);
+    } else if (value % 2 === 0) {
+      setMessage('Next 3 even numbers:');
+      setNextNumbers([value + 2, value + 4, value + 6]);
+    } else {
+      setMessage('Next 3 odd numbers:');
+      setNextNumbers([value + 2, value + 4, value + 6]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-container'>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Enter a number"
+        className='input-container'
+      />
+      {message && <p>{message}</p>}
+      {nextNumbers.length > 0 && (
+        <ul>
+          {nextNumbers.map((num, index) => (
+            <li key={index}>{num}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
